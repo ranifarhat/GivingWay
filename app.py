1c5c5a9f05943bfc8ee22bb94edb4c8cfc3bdc80
myname21-meet <myname21@meet.mit.edu>
2019-08-06 10:50:31 -0400

    made the second page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def challenge():
 	return render_template("ch_page.html")
 
+@app.route("/ch-list")
+def challenge_list():
+	return render_template("ch_lists.html")
+
 if __name__ == '__main__':
 	app.run(debug=True, port = 2000)
